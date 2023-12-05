8af3ec3025721b80a3ad9c34802511ee594e8ca3
ArcWolfStudio <arcwolfstudio@gmail.com>
2023-12-04 21:09:56 -0500

    Initial build

---
+++ src/main/java/com/arcwolf/tfcoinage/Tfcoinage.java
@@ -0,0 +1,48 @@
+package com.arcwolf.tfcoinage;
+
+import com.arcwolf.tfcoinage.init.ItemInit;
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Tfcoinage.MOD_ID)
+//@Mod.EventBusSubscriber(modid = Tfcoinage.MOD_ID,bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Tfcoinage {
+
+    public static final String MOD_ID = "tfcoinage";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Tfcoinage() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ItemInit.register(eventBus);
+
+        eventBus.addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // Some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+
+}
